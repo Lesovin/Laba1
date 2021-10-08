a6574615f8bc74797d47b4b327c266ff0ae58d96
Стефан Степанов <a1806430@yandex.ru>
2021-10-08 14:03:08 +0400

    разобрался с тестами, сделал несколько

--- Laba1/Set.cpp
+++ Laba1/Set.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "Set.h"
 #include <stdio.h>
 
--- test1/test.cpp
+++ test1/test.cpp
@@ -1,8 +1,54 @@
 #include "pch.h"
 #include"../Laba1/Set.h"
 #include"../Laba1/Set.cpp"
+ 
+TEST(Set, OperatorSquareBrackets)
+{
+	Set a;
+	a + 1;
+	ASSERT_EQ(1, a[0]);
+}
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(Set, OperatorAddition)
+{
+	Set a;
+	a + 1;
+	a + 2;
+	a + 3;
+	for (int i = 0; i < 3; i++)
+	{
+		ASSERT_EQ(i + 1, a[i]);
+	}
+}
+
+TEST(Set, OperatorAssignment)
+{
+	Set a;
+	Set b;
+	a + 1;
+	a + 2;
+	a + 3;
+	b = a;
+	for (int i = 0; i < 3; i++)
+	{
+		ASSERT_EQ(a[i], b[i]);
+	}
+}
+
+TEST(Set, OperatorAssignmentOfSets)
+{
+	Set a;
+	Set b;
+	Set c;
+	a + 1;
+	a + 2;
+	a + 3;
+	b + 4;
+	b + 5;
+	b + 6;
+	c = a + b;
+	for (int i = 0; i < 6; i++)
+	{
+		ASSERT_EQ(i + 1, c[i]);
+	}
 }

610cf095f536baaafb00d13ccdf4b695303266e9
Стефан Степанов <a1806430@yandex.ru>
2021-10-08 14:54:56 +0400

    Сделал тесты

--- test1/test.cpp
+++ test1/test.cpp
@@ -52,3 +52,67 @@ TEST(Set, OperatorAssignmentOfSets)
 		ASSERT_EQ(i + 1, c[i]);
 	}
 }
+
+TEST(Set, IntersectionOfSets)
+{
+	Set a;
+	Set b;
+	Set c;
+	a + 1;
+	a + 3;
+	b + 3;
+	b + 4;
+	c = a.Intersection(b);
+	ASSERT_EQ(3, c[0]);
+}
+
+TEST(Set, Subtraction)
+{
+	Set a;
+	a + 1;
+	a + 2;
+	a - 2;
+	ASSERT_EQ(1, a[0]);
+}
+
+TEST(Set, SubtractionOfSets)
+{
+	Set a;
+	Set b;
+	Set c;
+	a + 1;
+	a + 2;
+	a + 3;
+	b + 4;
+	b + 3;
+	b + 5;
+	c = a - b;
+	ASSERT_EQ(1, c[0]);
+	ASSERT_EQ(2, c[1]);
+	c = b - a;
+	ASSERT_EQ(4, c[0]);
+	ASSERT_EQ(5, c[1]);
+}
+
+TEST(Set, ExceptionInAdditionOperator)
+{
+	Set a;
+	a + 1;
+	ASSERT_THROW(a + 1, const char*);
+}
+
+TEST(Set, ExceptionInSubtractionOperator)
+{
+	Set a;
+	ASSERT_THROW(a - 1, const char*);
+}
+
+TEST(Set, ExceptionInSquareBracketsOperator)
+{
+	Set a;
+	a + 1;
+	ASSERT_THROW(a[-1], const char*);
+	ASSERT_THROW(a[2], const char*);
+	a - 1;
+	ASSERT_THROW(a[0], const char*);
+}
